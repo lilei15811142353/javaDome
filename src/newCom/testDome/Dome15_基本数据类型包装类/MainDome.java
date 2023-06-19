f4dc35634647f58da78fe876bee4b30939979937
lilei15811142353 <15811142353@163.com>
2023-06-19 15:40:31 +0800

    基本数据类型包装类

--- src/newCom/testDome/Dome15_基本数据类型包装类/MainDome.java
+++ src/newCom/testDome/Dome15_基本数据类型包装类/MainDome.java
@@ -16,5 +16,28 @@ public class MainDome {
         int i5 = i4.intValue();
         System.out.println(i5);
 
+        String num1 = "12";
+        int i6 = Integer.parseInt(num1);
+        System.out.println(i6);
+
+        int i7 = Integer.valueOf(num1);
+        System.out.println(i7);
+
+
+        // 注意
+        Integer x1 = new Integer(10);
+        Integer x2 = new Integer(10);
+        System.out.println(x1 == x2);
+        System.out.println(x1.equals(x2));
+
+        Integer x3 = 10;
+        Integer x4 = 10;
+        System.out.println(x3 == x4);
+        System.out.println(x3.equals(x4));
+
+        Integer x5 = 128;
+        Integer x6 = 128;
+        System.out.println(x5 == x6);
+        System.out.println(x5.equals(x6));
     }
 }
